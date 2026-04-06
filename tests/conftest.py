cf5d4eab79e911e79eca7bd76c92d22c734ca1f5
Jacob Larkin <jacob.larkin@tealdrones.com>
2026-04-06 22:47:19 +0000

    -

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,9 +17,17 @@ def enable_logging_propagation():
     """
     logger = logging.getLogger("dojo")
     previous_propagate = logger.propagate
+    previous_level = logger.level
+    previous_handlers = list(logger.handlers)
+    
     logger.propagate = True
+    logger.setLevel(logging.NOTSET)
+    
     yield
+    
     logger.propagate = previous_propagate
+    logger.setLevel(previous_level)
+    logger.handlers = previous_handlers
 
 
 @pytest.fixture
